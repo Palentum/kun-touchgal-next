d809dbee1b01501c26b1aba9481672eac03cd245
Palentum <79781425+Palentum@users.noreply.github.com>
2026-04-20 13:36:13 +0800

    fix(random): use crypto.randomInt for tokens

--- utils/random.ts
+++ utils/random.ts
@@ -1,3 +1,5 @@
+import { randomInt } from 'crypto'
+
 export const randomNum = (lowerValue: number, upperValue: number) => {
   return Math.floor(Math.random() * (upperValue - lowerValue + 1) + lowerValue)
 }
@@ -26,8 +28,7 @@ export const generateRandomString = (length: number) => {
   const charset = '023456789abcdefghjkmnopqrstuvwxyz'
   let code = ''
   for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * charset.length)
-    code += charset[randomIndex]
+    code += charset[randomInt(charset.length)]
   }
   return code.toUpperCase()
 }
